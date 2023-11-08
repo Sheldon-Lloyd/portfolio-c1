54489b9ee806af5f0d1efcf1b81b3d9868c80393
Sheldon Lloyd <slloyd227@gmail.com>
2023-11-08 14:48:45 -0500

    Made feature list async to prevent lagging

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+
 const modalButton = document.querySelector('.modal-button');
 const closeButton = document.querySelector('.close');
 const modal = document.querySelector('.modal');
@@ -17,5 +18,56 @@ closeButton.addEventListener('click',function (e) {
 document.addEventListener('keydown', function(event) {
   if (event.keyCode === 27) {
     modal.classList.add('is-hidden');
+  }
+});
+
+// DOM Elements and Global Variables
+const addTaskBtn = document.getElementById('add-feature');
+const input = document.querySelector('.input-feature');
+const taskList = document.querySelector('#feature-list');
+
+let taskId = 0;
+
+// Add a new task to the list using async as the old way lagged
+async function addTask (task) {
+  const taskItem = document.createElement('li');
+  taskItem.innerHTML = `
+    <input class="task-check" type="checkbox" id="task-${taskId}">
+    <label class="task-check-label" for="task-${taskId}">${task}</label>
+    <button type="button" class="close btn-close" aria-label="Close" data-task-id="${taskId}">&#215;</button>
+  `;
+  taskList.appendChild(taskItem);
+  taskId++;
+}
+
+// Remove a task from the list using async so the it does not get in the way
+async function removeTask (taskId) {
+  const taskItem = document.querySelector(`#task-${taskId}`).parentNode;
+  taskList.removeChild(taskItem);
+}
+
+// Handle Add Task button click
+addTaskBtn.addEventListener('click', () => {
+  const task = input.value.trim();
+
+  if (task !== '') {
+    addTask(task);
+    input.value = '';
+  }
+});
+
+// Handle completing a task and removing it from the list
+taskList.addEventListener('click', (e) => {
+  const target = e.target;
+  if (target.matches('.task-check')) {
+    const label = target.parentNode.querySelector('label');
+    if (target.checked) {
+      label.classList.add('text-decoration-line-through');
+    } else {
+      label.classList.remove('text-decoration-line-through');
+    }
+  } else if (target.matches('.btn-close')) {
+    const taskId = target.getAttribute('data-task-id');
+    removeTask(taskId);
   }
 });
